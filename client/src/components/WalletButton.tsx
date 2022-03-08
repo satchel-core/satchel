f05aafba52f358f2f10fcdd162fc78b985cb7aca
Darya Kaviani <daryakaviani@berkeley.edu>
2022-03-07 16:54:15 -0800

    almost done w metamask

--- client/src/components/WalletButton.tsx
+++ client/src/components/WalletButton.tsx
@@ -3,10 +3,10 @@ import {
   Button,
   Icon,
 } from "@chakra-ui/react"
-import { handleClick } from "../utils/common";
+import { handleClick, stripAddress } from "../utils/common";
 import { NextRouter, useRouter } from "next/router";
 import Web3 from "web3";
-const web3 = new Web3(Web3.givenProvider);
+let web3: Web3 | undefined = undefined; // Will hold the web3 instance
 
 type WalletButtonProps = {
   walletName: string
@@ -16,59 +16,104 @@ export const WalletButton: FunctionComponent<WalletButtonProps> = ({walletName})
   const router = useRouter();
   // TODO: Remove this later
   const address_org = "6bf76B2668fF5446fbaDCb94231E2A44ba077bd6"
+  
+  const handleLogin = async () => {
+    if (!(window as any).ethereum) {
+      window.alert('Please install MetaMask first.');
+      return;
+    }
+    if (!web3) {
+      try {
+        // Request account access if needed
+        await (window as any).ethereum.enable();
 
-  function handleLogin(publicAddress: any, router: NextRouter) {
-    // --snip--
-    fetch(`${process.env.REACT_APP_BACKEND_URL}/users?publicAddress=${publicAddress}`)
-    .then(response => response.json())
-    // If yes, retrieve it. If no, create it.
-    .then(
-      users => (users.length ? users[0] : this.handleSignup(publicAddress))
+        // We don't know window.web3 version, so we use our own instance of Web3
+        // with the injected provider given by MetaMask
+        web3 = new Web3((window as any).ethereum);
+      } catch (error) {
+        window.alert('You need to allow MetaMask.');
+        return;
+      }
+    }
+
+    const coinbase = await web3.eth.getCoinbase();
+    if (!coinbase) {
+      window.alert('Please activate MetaMask first.');
+      return;
+    }
+
+    const publicAddress = coinbase.toLowerCase();
+
+    // Look if org with current publicAddress is already present on backend
+    fetch(
+      `${
+        process.env.REACT_APP_SERVER_URL
+      }/api/org/?address=${stripAddress(publicAddress)}`
     )
-    // Popup MetaMask confirmation modal to sign message
-    .then(this.handleSignMessage)
-    // Send signature to back end on the /auth route
-    .then(this.handleAuthenticate)
-    .then(user => {
-      user.nonce = Math.floor(Math.random() * 1000000);
-      return user.save();
-    })
-    return this.handleClick("/org/"+[publicAddress], router);
-    // --snip--
-  }
+      .then(response => response.json())
+      // If yes, retrieve it. If no, create it.
+      .then(users =>
+        users.length ? users[0] : handleSignup(publicAddress)
+      )
+      // Popup MetaMask confirmation modal to sign message
+      .then(handleSignMessage)
+      // Send signature to backend on the /auth route
+      .then(handleAuthenticate)
+      .then(handleClick(publicAddress, router))
+      // Pass accessToken back to parent component (to save it in localStorage)
+      // .then(onLoggedIn)
+      // .catch(err => {
+      //   window.alert(err);
+      //   this.setState({ loading: false });
+      // });
+  };
+
   
-  function handleSignup(publicAddress) {
-    fetch(`${process.env.REACT_APP_BACKEND_URL}/users`, {
-      body: JSON.stringify({ publicAddress }),
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      method: 'POST'
-    }).then(response => response.json());
-  }
-  
-  function handleAuthenticate(publicAddress: any, signature: any ) {
-    fetch(`${process.env.REACT_APP_BACKEND_URL}/auth`, {
-      body: JSON.stringify({ publicAddress, signature }),
+  const handleSignup = (publicAddress: string) => {
+    return fetch(`${process.env.REACT_APP_SERVER_URL}/createOrg`, {
+      body: JSON.stringify({ name: "Your Mom", publicAddress, schools: [] }),
       headers: {
         'Content-Type': 'application/json'
       },
       method: 'POST'
     }).then(response => response.json());
   };
+  
+  const handleAuthenticate = ({
+    publicAddress,
+    signature
+  }: {
+    publicAddress: string;
+    signature: string;
+  }) =>
+    fetch(`${process.env.REACT_APP_SERVER_URL}/auth`, {
+      body: JSON.stringify({ publicAddress, signature }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST'
+    }).then(response => response.json());
 
-  function handleSignMessage({ publicAddress, nonce }) {
-    return new Promise((resolve, reject) =>
-      web3.personal.sign(
-        web3.fromUtf8(`I am signing my one-time nonce: ${nonce}`),
+  const handleSignMessage = async ({
+    publicAddress,
+    nonce
+  }: {
+    publicAddress: string;
+    nonce: string;
+  }) => {
+    try {
+      const signature = await web3!.eth.personal.sign(
+        `I am signing my one-time nonce: ${nonce}`,
         publicAddress,
-        (err, signature) => {
-          if (err) return reject(err);
-          return resolve({ publicAddress, signature });
-        }
-      )
-    );
-  }
+        '' // MetaMask will ignore the password argument here
+      );
+
+      return { publicAddress, signature };
+    } catch (err) {
+      throw new Error('You need to sign the message to be able to log in.');
+    }
+  };
+
 
   return <Button 
             isFullWidth 
@@ -77,7 +122,7 @@ export const WalletButton: FunctionComponent<WalletButtonProps> = ({walletName})
             color="black" 
             variant="outline" 
             leftIcon={<Icon></Icon>} 
-            onClick={handleLogin(address_org, router)}>
+            onClick={handleLogin}>
               {walletName}
           </Button>;
 }
--- server/src/routes/orgRouter.ts
+++ server/src/routes/orgRouter.ts
@@ -1,7 +1,11 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import Org from '../models/Org';
 import * as dotenv from 'dotenv';
 import School from '../models/School';
+import * as ethUtil from 'ethereumjs-util';
+import * as sigUtil from 'eth-sig-util';
+import * as jwt from 'jsonwebtoken';
+import { config } from '../config';
 
 dotenv.config();
 
@@ -39,7 +43,7 @@ router.get('/allOrgs', async (req, res) => {
     return res.status(200).json({ success: true, orgs });
 });
 
-router.get('/getSchools', async(req, res) => {
+router.get('/getSchools', async (req, res) => {
     const { orgAddress } = req.query;
     let schools = [];
     try {
@@ -61,8 +65,8 @@ router.post('/createOrg', async (req, res) => {
         });
     } else if (!schools) {
         return res.status(400).json({
-            email: "Schools not found"
-        })
+            email: 'Schools not found',
+        });
     } else if (!address) {
         return res.status(400).json({
             address: 'Address not found',
@@ -79,6 +83,92 @@ router.post('/createOrg', async (req, res) => {
 
     return res.status(200).json({ success: true });
 });
+
+export const create = (req: Request, res: Response, next: NextFunction) => {
+    const { signature, publicAddress } = req.body;
+    if (!signature || !publicAddress)
+        return res.status(400).send({ error: 'Request should have signature and publicAddress' });
+
+    return (
+        Org.findOne({ where: { publicAddress } })
+            ////////////////////////////////////////////////////
+            // Step 1: Get the user with the given publicAddress
+            ////////////////////////////////////////////////////
+            .then((user) => {
+                if (!user)
+                    return res.status(401).send({
+                        error: `User with publicAddress ${publicAddress} is not found in database`,
+                    });
+                return user;
+            })
+            ////////////////////////////////////////////////////
+            // Step 2: Verify digital signature
+            ////////////////////////////////////////////////////
+            .then((user) => {
+                if (!(user instanceof Org)) {
+                    // Should not happen, we should have already sent the response
+                    throw new Error('User is not defined in "Verify digital signature".');
+                }
+
+                const msg = `I am signing my one-time nonce: ${user.nonce}`;
+
+                // We now are in possession of msg, publicAddress and signature. We
+                // will use a helper from eth-sig-util to extract the address from the signature
+                const msgBufferHex = ethUtil.bufferToHex(Buffer.from(msg, 'utf8'));
+                const address = sigUtil.recoverPersonalSignature({
+                    data: msgBufferHex,
+                    sig: signature,
+                });
+
+                // The signature verification is successful if the address found with
+                // sigUtil.recoverPersonalSignature matches the initial publicAddress
+                if (address.toLowerCase() === publicAddress.toLowerCase()) {
+                    return user;
+                } else {
+                    return res.status(401).send({ error: 'Signature verification failed' });
+                }
+            })
+            ////////////////////////////////////////////////////
+            // Step 3: Generate a new nonce for the user
+            ////////////////////////////////////////////////////
+            .then((user) => {
+                if (!(user instanceof Org)) {
+                    // Should not happen, we should have already sent the response
+
+                    throw new Error('User is not defined in "Generate a new nonce for the user".');
+                }
+
+                user.nonce = Math.floor(Math.random() * 10000);
+                return user.save();
+            })
+            ////////////////////////////////////////////////////
+            // Step 4: Create JWT
+            ////////////////////////////////////////////////////
+            .then((user) => {
+                return new Promise<string>((resolve, reject) =>
+                    // https://github.com/auth0/node-jsonwebtoken
+                    jwt.sign(
+                        {
+                            payload: {
+                                id: user.id,
+                                publicAddress,
+                            },
+                        },
+                        config.secret,
+                        {},
+                        (err, token) => {
+                            if (err) {
+                                return reject(err);
+                            }
+                            return resolve(token || ''); // TODO: fix this
+                        }
+                    )
+                );
+            })
+            .then((accessToken) => res.json({ accessToken }))
+            .catch(next)
+    );
+};
 
 module.exports = router;
 export default router;
